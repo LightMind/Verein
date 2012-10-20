91e7a8c63d10f3b32a0dadc5d6bee3c749cdba75
Joe <waltherj@web.de>
2012-10-20 23:51:50 +0200

    formatierungen

--- src/plan/Horse.java
+++ src/plan/Horse.java
@@ -8,33 +8,38 @@ import util.Factory;
 import util.SmartHashMap;
 
 public class Horse {
-	public final int id;
+
+    public final int id;
     public String name;
     public Set<Lesson> lessons = new HashSet<Lesson>();
-    public Set<Student> students = new HashSet<Student>();    
-    public Map<Lesson,Set<Student>> riders = new SmartHashMap<Lesson, Set<Student>>(
-			new Factory<Set<Student>>() {
-				@Override
-				public Set<Student> create() {
-					return new HashSet<Student>();
-				}
-			});;
-    public Map<Student,Set<Lesson>> inLesson = new SmartHashMap<Student, Set<Lesson>>(
-			new Factory<Set<Lesson>>() {
-				@Override
-				public Set<Lesson> create() {
-					return new HashSet<Lesson>();
-				}
-			});;
+    public Set<Student> students = new HashSet<Student>();
+    public Map<Lesson, Set<Student>> riders = new SmartHashMap<Lesson, Set<Student>>(
+            new Factory<Set<Student>>() {
 
-    public Horse(String name, int id){
+                @Override
+                public Set<Student> create() {
+                    return new HashSet<Student>();
+                }
+            });
+    ;
+    public Map<Student, Set<Lesson>> inLesson = new SmartHashMap<Student, Set<Lesson>>(
+            new Factory<Set<Lesson>>() {
+
+                @Override
+                public Set<Lesson> create() {
+                    return new HashSet<Lesson>();
+                }
+            });
+
+    ;
+
+    public Horse(String name, int id) {
         this.name = name;
         this.id = id;
     }
 
     // Accessors //
-
-    public String getName(){
+    public String getName() {
         return name;
     }
 
@@ -46,66 +51,71 @@ public class Horse {
         return new HashSet<Student>(students);
     }
 
-    public Set<Student> getStudents(Lesson l){
+    public Set<Student> getStudents(Lesson l) {
         return new HashSet<Student>(riders.get(l));
     }
 
-    public Set<Lesson> getLessons(Student st){
+    public Set<Lesson> getLessons(Student st) {
         return new HashSet<Lesson>(inLesson.get(st));
     }
 
     // mutators //
-    
-    public void assign(Lesson l,Student st){
+    public void assign(Lesson l, Student st) {
         lessons.add(l);
         students.add(st);
         inLesson.get(st).add(l);
         riders.get(l).add(st);
     }
-    
-    public void unassign(Lesson l,Student st){
+
+    public void unassign(Lesson l, Student st) {
         riders.get(l).remove(st);
         Set<Lesson> lset = inLesson.get(st);
         lset.remove(l);
-        if ( lset.isEmpty() ){
+        if (lset.isEmpty()) {
             students.remove(st);
         }
         Set<Student> rset = riders.get(l);
-        if ( rset.isEmpty() ){
+        if (rset.isEmpty()) {
             lessons.remove(l);
         }
     }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
 
-	@Override
-	public String toString() {
-		return "Horse [id=" + id + ", name=" + name + "]";
-	}
+    @Override
+    public String toString() {
+        return "Horse [id=" + id + ", name=" + name + "]";
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Horse other = (Horse) obj;
-		if (id != other.id)
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Horse other = (Horse) obj;
+        if (id != other.id) {
+            return false;
+        }
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        return true;
+    }
 }
--- src/plan/Lesson.java
+++ src/plan/Lesson.java
@@ -10,167 +10,187 @@ import java.util.Map;
 import java.util.Set;
 
 public class Lesson implements Comparable<Lesson> {
-	public final int id;
-	
-	public Set<Student> students = new HashSet<Student>();
-	public Map<Student, Horse> riders = new HashMap<Student, Horse>();
-	public Set<Teacher> teachers = new HashSet<Teacher>();
-	public Calendar beginn = new GregorianCalendar();
-	public Calendar end = new GregorianCalendar();
-	public LessonType lessonType = null;
-	public Place place = null;
 
-	public Lesson(int id){
-		this.id = id;
-	}
-	
-	// mutators //
+    public final int id;
+    public Set<Student> students = new HashSet<Student>();
+    public Map<Student, Horse> riders = new HashMap<Student, Horse>();
+    public Set<Teacher> teachers = new HashSet<Teacher>();
+    public Calendar beginn = new GregorianCalendar();
+    public Calendar end = new GregorianCalendar();
+    public LessonType lessonType = null;
+    public Place place = null;
 
-	public void setPlace(Place p) {
-		if (place != null) {
-			place.unassign(this);
-		}
-		p.assign(this);
-		this.place = p;
-	}
+    public Lesson(int id) {
+        this.id = id;
+    }
 
-	public void setLessonType(LessonType lt) {
-		if (lessonType != null) {
-			lessonType.unassign(this);
-		}
-		lt.assign(this);
-		lessonType = lt;
-	}
+    // mutators //
+    public void setPlace(Place p) {
+        if (place != null) {
+            place.unassign(this);
+        }
+        p.assign(this);
+        this.place = p;
+    }
 
-	public void setEnd(Calendar c) {
-		end.setTime(c.getTime());
-	}
+    public void setLessonType(LessonType lt) {
+        if (lessonType != null) {
+            lessonType.unassign(this);
+        }
+        lt.assign(this);
+        lessonType = lt;
+    }
 
-	public void setBeginn(Calendar c) {
-		beginn.setTime(c.getTime());
-		lessonType.notify(this);
-		place.notify(this);
-	}
+    public void setEnd(Calendar c) {
+        end.setTime(c.getTime());
+    }
 
-	public void assign(Teacher t) {
-		teachers.add(t);
-		t.assign(this);
-	}
+    public void setBeginn(Calendar c) {
+        beginn.setTime(c.getTime());
+        lessonType.notify(this);
+        place.notify(this);
+    }
 
-	public void unassign(Teacher t) {
-		teachers.remove(t);
-		t.unassign(this);
-	}
+    public void assign(Teacher t) {
+        teachers.add(t);
+        t.assign(this);
+    }
 
-	public boolean addStudent(Student student) {
-		student.assign(this);
-		return students.add(student);
-	}
+    public void unassign(Teacher t) {
+        teachers.remove(t);
+        t.unassign(this);
+    }
 
-	public void removeStudent(Student st) {
-		if (students.contains(st)) {
-			st.unassign(this);
-			students.remove(st);
-		}
-	}
+    public boolean addStudent(Student student) {
+        student.assign(this);
+        return students.add(student);
+    }
 
-	public void assignHorse(Student st, Horse h) {
-		if (!students.contains(st)){
-			addStudent(st);
-		}
-		if (null != riders.get(st)) {
-			Horse hOld = riders.get(st);
-			hOld.unassign(this, st);
-			st.unassign(this, hOld);
-		}
-		st.assign(this, h);
-		h.assign(this, st);
-		riders.put(st, h);
-	}
+    public void removeStudent(Student st) {
+        if (students.contains(st)) {
+            st.unassign(this);
+            students.remove(st);
+        }
+    }
 
-	// accessors //
+    public void assignHorse(Student st, Horse h) {
+        if (!students.contains(st)) {
+            addStudent(st);
+        }
+        if (null != riders.get(st)) {
+            Horse hOld = riders.get(st);
+            hOld.unassign(this, st);
+            st.unassign(this, hOld);
+        }
+        st.assign(this, h);
+        h.assign(this, st);
+        riders.put(st, h);
+    }
 
-	public Calendar getEnd() {
-		Calendar r = new GregorianCalendar();
-		r.setTime(end.getTime());
-		return r;
-	}
+    // accessors //
+    public Calendar getEnd() {
+        Calendar r = new GregorianCalendar();
+        r.setTime(end.getTime());
+        return r;
+    }
 
-	public List<Student> getStudents() {
-		return new ArrayList<Student>(students);
-	}
+    public List<Student> getStudents() {
+        return new ArrayList<Student>(students);
+    }
 
-	public int numberOfStudents() {
-		return students.size();
-	}
+    public int numberOfStudents() {
+        return students.size();
+    }
 
-	public Horse rides(Student st) {
-		return riders.get(st);
-	}
+    public Horse rides(Student st) {
+        return riders.get(st);
+    }
 
-	public List<Horse> getHorses() {
-		return new ArrayList<Horse>(riders.values());
-	}
+    public List<Horse> getHorses() {
+        return new ArrayList<Horse>(riders.values());
+    }
 
-	public Calendar getBeginn() {
-		Calendar r = new GregorianCalendar();
-		r.setTime(beginn.getTime());
-		return r;
-	}
+    public Calendar getBeginn() {
+        Calendar r = new GregorianCalendar();
+        r.setTime(beginn.getTime());
+        return r;
+    }
 
-	public Set<Teacher> getTeachers() {
-		return new HashSet<Teacher>(teachers);
-	}
+    public Set<Teacher> getTeachers() {
+        return new HashSet<Teacher>(teachers);
+    }
 
-	@Override
-	public int compareTo(Lesson arg0) {
-		int c1 = getBeginn().compareTo(arg0.getBeginn());
-		if ( c1 == 0){
-			int c2 = end.compareTo(arg0.getEnd());
-			if (c2 == 0){
-				return id - arg0.id;
-			}
-			return c2;
-		}
-		return c1;
-	}
+    @Override
+    public int compareTo(Lesson arg0) {
+        int c1 = getBeginn().compareTo(arg0.getBeginn());
+        if (c1 == 0) {
+            int c2 = end.compareTo(arg0.getEnd());
+            if (c2 == 0) {
+                return id - arg0.id;
+            }
+            return c2;
+        }
+        return c1;
+    }
 
-	@Override
-	public String toString() {
-		return "Lesson [id=" + id + "]";
-	}
+    public boolean isOverlapping(Lesson lesson) {
+        Calendar s1, s2, e1, e2;
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((beginn == null) ? 0 : beginn.hashCode());
-		result = prime * result + ((end == null) ? 0 : end.hashCode());
-		result = prime * result + id;
-		return result;
-	}
+        s1 = beginn;
+        s2 = lesson.getBeginn();
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Lesson other = (Lesson) obj;
-		if (beginn == null) {
-			if (other.beginn != null)
-				return false;
-		} else if (!beginn.equals(other.beginn))
-			return false;
-		if (end == null) {
-			if (other.end != null)
-				return false;
-		} else if (!end.equals(other.end))
-			return false;
-		if (id != other.id)
-			return false;
-		return true;
-	}
+        e1 = end;
+        e2 = lesson.getEnd();
+
+        return ((s1.before(s2) && e1.after(s2))
+                || (s1.after(s2) && s1.before(e2))
+                || s1.equals(s2));
+    }
+
+    @Override
+    public String toString() {
+        return "Lesson [id=" + id + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((beginn == null) ? 0 : beginn.hashCode());
+        result = prime * result + ((end == null) ? 0 : end.hashCode());
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Lesson other = (Lesson) obj;
+        if (beginn == null) {
+            if (other.beginn != null) {
+                return false;
+            }
+        } else if (!beginn.equals(other.beginn)) {
+            return false;
+        }
+        if (end == null) {
+            if (other.end != null) {
+                return false;
+            }
+        } else if (!end.equals(other.end)) {
+            return false;
+        }
+        if (id != other.id) {
+            return false;
+        }
+        return true;
+    }
 }
